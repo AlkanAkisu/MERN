2b4997b77d878c5e74733389bf37a0045f858265
AlkyAky56 <=>
2020-05-17 04:04:38 +0300

    Init

---
+++ server.js
@@ -0,0 +1,11 @@
+//82fca4f67a343b53f39b1ac74e19cce6558aea88
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', function (res, req) {
+	req.send('Hello World');
+});
+
+app.listen(PORT, () => console.log('Listening at ' + PORT));
