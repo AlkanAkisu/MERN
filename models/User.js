b0c70af90afdc31ce8dd16d46e5fb0ca19dd8fb3
AlkyAky56 <=>
2020-05-21 14:52:07 +0300

    We can add users now

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+
+
 const UserSchema = new mongoose.Schema({
 	name: {
 		type: String,
@@ -22,6 +24,7 @@ const UserSchema = new mongoose.Schema({
 		default: Date.now,
 	},
 });
+
 
 
 module.exports = User = mongoose.model('user',UserSchema);
